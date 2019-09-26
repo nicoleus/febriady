b40b86eb37e395679052899aa7925c907e4321a8
Nicoleus1 <55134353+nicoleus@users.noreply.github.com>
2019-09-26 11:20:59 +0700

    brute.py

--- brute.py
+++ brute.py
@@ -115,7 +115,6 @@ def main():
         print RR+" wordlist tidak ada yg cocok LAE..."
         print " "
 	
-
   try:
  	sys.stdout.write(GG+"\n[\033[91m+\033[92m]\033[91;1m [\033[97m"+email_target+"\033[91m]\033[90m Mencoba ==> \033[91m[\033[90;1m"+iqbalz_password)
 	sys.stdout.flush()
